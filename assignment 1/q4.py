dd70b19304b62a709c1e53f51d555063dd29d998
Alexpreo <alexander.preo@gmail.com>
2023-01-27 13:13:00 -0800

    beggings of program done. just need pre spacing

--- assignment 1/q4.py
+++ assignment 1/q4.py
@@ -1,1 +1,11 @@
-    
+height = int(input("What is the height of the pyramid?\n"))
+
+if height < 2 or height > 9:
+    print("PyNum cannot help you!")
+else:
+    for i in range(1, height + 1):
+        for j in range(1, i + 1):
+            print(j, end=" ")
+        for k in range(i - 1, 0, -1):
+            print(k, end=" ")
+        print()
