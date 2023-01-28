e90efe9c60013b64cd0f962dc6268ce67e2ddd8e
Alexpreo <alexander.preo@gmail.com>
2023-01-27 17:37:27 -0800

    no changes made. still 100 percent

--- assignment 1/q1.py
+++ assignment 1/q1.py
@@ -16,7 +16,7 @@ while True:
 
 positive = ["Good", "I'm great", "I'm good", "Fine"]
 negative = ["Bad", "Not Okay", "I'm not feeling good"]
-    
+
 question3 = input("How are you doing today?\n")
     
 if question3 in positive:
