1d12a9e1aeba99e103aca963f24445852e3a8bb6
Alexpreo <alexander.preo@gmail.com>
2023-02-13 19:04:35 -0800

    returning 48/60 no clue what's wrong

--- assignment 2/q3.py
+++ assignment 2/q3.py
@@ -1,23 +1,31 @@
 def palindromeChecker(source):
-    word = toLower(source)
+    word = converter(source)
     word_len = len(word)
     for i in range(word_len//2):
-        if word[i] != word[word_len-i-1]:
-            return 'No'
-    return 'Yes'
+        if word[i] == word[word_len-i-1]:
+            return 'Yes'
+    return 'No'
+    # word = converter(source)
+    # reverse = word[::-1]
+    # if word == reverse:
+    #     return 'Yes'
+    # return 'No'
     
-    
-def toLower(source):
+def converter(source):
     lowercase = ''
     for char in source:
         if ord(char) >= 65 and ord(char) <= 90:
             char = ord(char)+32
             to_letters = chr(char)
             lowercase += to_letters
-        elif:
-            
+        elif ord(char) == 39 or ord(char) == 44 or ord(char) == 8217 or ord(char) == 32:
+            char = ord(char)
+            char = 0
+            to_letters = chr(char)
+            lowercase += to_letters
         else:
             lowercase += char
+    #print(lowercase)
     return lowercase
             
 source = str(input())
