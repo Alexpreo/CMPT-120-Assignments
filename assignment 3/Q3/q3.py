9cf5eb900e9a32785e7b1c3cc9fa56073d21d652
Alexpreo <alexander.preo@gmail.com>
2023-03-17 21:32:45 -0700

    doen everything except 20 points on q3 but fuck it

--- assignment 3/Q3/q3.py
+++ assignment 3/Q3/q3.py
@@ -1,36 +1,33 @@
 import os
-import sys
 
-# define a function to calculate the score of a document with respect to a query
-def calculate_score(query, document):
-    # split the query and document into words
+def count_words(filename):
+    with open(filename, 'r') as f:
+        words = f.read().split()
+        return {word.strip('\'",.():;-_') for word in words}
+
+def score_document(query, filename):
     query_words = query.split()
-    with open(document, 'r') as file:
-        document_words = file.read().split()
-
-    # calculate the score as the number of common words between query and document
-    score = len(set(query_words) & set(document_words))
+    document_words = count_words(filename)
+    score = 0
+    for word in query_words:
+        if word in document_words:
+            score += 1
     return score
 
-# get the total number of documents
+def find_highest_score(query, n):
+    max_score = -1
+    max_filename = ''
+    for i in range(1, n+1):
+        filename = os.path.join(os.path.dirname(__file__), str(i) + '.txt')
+        if os.path.isfile(filename):
+            score = score_document(query, filename)
+            if score > max_score:
+                max_score = score
+                max_filename = filename
+            elif score == max_score and filename < max_filename:
+                max_filename = filename
+    return int(os.path.splitext(os.path.basename(max_filename))[0])
+
 n = int(input())
-
-# create a list of document names
-documents = [f"{i}.txt" for i in range(1, n+1)]
-
-# get the query from the user
-query = input()
-
-# calculate the score for each document and store the max score and corresponding document
-max_score = -1
-max_document = ''
-for document in documents:
-    score = calculate_score(query, document)
-    if score > max_score:
-        max_score = score
-        max_document = document
-    elif score == max_score:
-        max_document = min(max_document, document)
-
-# print the name of the document with the maximum score
-print(os.path.splitext(max_document)[0])
+query = input().strip()
+print(find_highest_score(query, n))
